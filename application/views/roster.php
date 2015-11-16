5e47dd6739b434fe9580311014761d61a88ec26d
tom-bui <toandbui@hotmail.com>
2015-11-15 17:57:12 -0800

    styling added by Lukasz, who still has not fixed his xampp

--- application/views/roster.php
+++ application/views/roster.php
@@ -1,19 +1,19 @@
-<a id="button" title="button" class="btn btn-default" href="/player/editmode">{editmode}</a>
+<a id="buttonEdit" title="button" class="btn btn-default" href="/player/editmode">{editmode}</a>
 {addbutton}
-Sort By
-<form action="/player/ordertype" method="post">
-    <select name="ordertype">
+<form action="/player/ordertype" method="post" >
+    <select name="ordertype" class="form-control roster-select">
         <option value="1">Name</option>
         <option value="2">Position</option>
         <option value="3">Jersey Number</option>
     </select>
-    <select name="layout">
+    <select name="layout" class="form-control roster-select">
         <option value="1">Gallery</option>
         <option value="2">Table</option>
     </select>
-    <input type="submit" value="Go" />
+    <input class= "btn btn-default" type="submit" value="Go" />
 </form>
 {thetable}
+
 {links}
 <script>
 $('[name=ordertype]').val('{ordertype}');
--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,6 +1,13 @@
-<h1 class="section-heading">Seahawks</h1>
-<p class="lead section-lead">You know what they say about men with big shoes</p>
-<p class="section-paragraph">
+<div class="container rounded-header" style="background:#0e0e52">
+<h1 class="section-heading" style="margin:20px 0; margin-left:20px;color:#fff;font-weight:light;">Seahawks</h1>
+</div>
+
+<div class="container rounded under-header">
+
+    <p class="lead section-lead"><i>"You know what they say about men with big shoes"</i>
+        <span style="font-size:.2em;margin-left:400px;"><br>- Jim Perry</span></p>
+    
+    <p class="section-paragraph">
     The Seattle Seahawks are a professional American football franchise based in Seattle, Washington.
     They are members of the West division of the National Football Conference (NFC) in the National
     Football League (NFL). The Seahawks joined the NFL in 1976 as an expansion team along with the Tampa
@@ -10,4 +17,5 @@
     Husky Stadium (1994, 2000-2001).
     The Seahawks are the only NFL franchise based in the Pacific Northwest region of North America and thus attract
     support from a wide geographical area, including Oregon, Montana, Idaho, and Alaska, as well as Canadian fans in British Columbia and Alberta.[5]
-</p>
+    </p
+</div>
