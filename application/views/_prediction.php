ce746a3b9a8686f6d7197e6bacdf2a90a5491328
Lukasz Pacyk <lpacyk31@gmail.com>
2015-12-05 22:18:13 -0800

    added selection box

---
+++ application/views/_prediction.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+
+
+<a href="/player/{handletype}/{id}" data-lightbox="gallery"
+   data-title="#{jersey} {last_name}, {first_name} - {position}">
+    <img width="312" height="312" src="/img/roster/{image_name}"/>
+</a>
+<p style="color:#fff;background-color:#4b8d27;">#{jersey} {last_name}, {first_name}</p>
